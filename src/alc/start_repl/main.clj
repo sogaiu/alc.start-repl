0b84bb7c77b12dce9127a4dfad23c5541a0c5fe8
sogaiu <983021772@users.noreply.github.com>
2020-01-12 16:52:16 -0800

    Improve cmd line handling

--- src/alc/start_repl/main.clj
+++ src/alc/start_repl/main.clj
@@ -2,37 +2,48 @@
   (:require
    [alc.start-repl.core :as as.c]
    [alc.start-repl.impl.cli :as asi.c]
-   [alc.start-repl.impl.exit :as asi.e])
+   [alc.start-repl.impl.exit :as asi.e]
+   [clojure.java.io :as cji])
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 
 (def cli-options
   [["-a" "--agent-jar AGENT-JAR" "Agent jar path"
-    :default nil]
+    :default nil
+    :validate [#(let [thing (cji/file %)]
+                  (and (.exists thing)
+                    (.isFile thing)))
+               "Must be an existing file"]]
    ["-d" "--proj-dir PROJ-DIR" "Project directory"
-    :default nil] ; XXX: possibly validate as existing directory?
+    :default nil
+    :validate [#(let [thing (cji/file %)]
+                  (and (.exists thing)
+                    (.isDirectory thing)))
+               "Must be an existing directory"]]
    ["-g" "--debug DEBUG" "Debug output"]
-   ;; XXX: fill this out?
-   ["-h" "--help"]
+   ["-h" "--help" "Show usage"]
    ["-i" "--pid PID" "Process identifier"
     :default nil
     :parse-fn #(Integer/parseInt %)]
    ["-p" "--port PORT" "Port number"
     :default nil
     :parse-fn #(Integer/parseInt %)
-    :validate [#(< 1024 % 65536) "Must be a number between 1025 and 65536"]]])
+    :validate [#(< 1024 % 65536)
+               "Must be a number between 1025 and 65535 inclusive"]]])
 
 (defn -main
   [& args]
   (let [opts
         (asi.c/parse-opts args cli-options)
-        errors (:errors opts)]
-    (if-not errors
+        {:keys [:errors :summary]} opts]
+    (if errors
       (do
-        (as.c/start-repl (:options opts))
-        (asi.e/exit 0))
-      (do
-        ;; XXX
         (println errors)
-        (asi.e/exit 1)))))
+        (println summary)
+        (asi.e/exit 1))
+      (do
+        (if (:help (:options opts))
+          (println summary)
+          (as.c/start-repl (:options opts)))
+        (asi.e/exit 0)))))
